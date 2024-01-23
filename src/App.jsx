e1e6bd19d415c0794f923efb32aee62e1eeb3b92
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-24 03:57:49 +0600

    create a vite react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <nav>
+          <div className="logo">
+            <img src="./../public/images/brand_logo.png" alt="" />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
