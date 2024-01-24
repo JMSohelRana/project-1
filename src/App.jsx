e2be8be32ffaf16dce156bae93330b13daf367cd
JMSohelRana <jmsohelrana04@gmail.com>
2024-01-24 06:16:07 +0600

    navbar section added with css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import brandImage from "../public/images/brand_logo.png";
+import "./App.css";
 function App() {
   return (
     <>
-      <div className="">
+      <div className="container">
         <nav>
           <div className="logo">
-            <img src="./../public/images/brand_logo.png" alt="" />
+            <img src={brandImage} alt="logo" />
           </div>
+          <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+          </ul>
+          <button className="button">Login</button>
         </nav>
       </div>
     </>
